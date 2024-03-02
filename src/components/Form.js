b1c87e3f5b57c65a4d198a8f928dfcc0d72b9fd8
Negar Lashgari <negar.lashgari@gmail.com>
2024-03-01 22:44:49 -0500

    updating form so deployed site works

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,17 +5,18 @@ import emailjs from 'emailjs-com';
 const Form = () => {
   const formRef = useRef();
   const [message, setMessage] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault(); //prevent the page to refresh
 
     emailjs
-      .sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, formRef.current, process.env.REACT_APP_EMAILJS_USER_ID)
+      .sendForm('service_kje18ll', 'template_00low3l', formRef.current, 'icqODf3FnuhSYIFq6')
       .then((result) => {
-        // console.log(result.text);
+        console.log(result.text);
         setMessage('Thank you!');
       })
       .catch((error) => {
-        // console.log(error.text);
+        console.log(error.text);
         setMessage('There was an error sending the email. Please try again later.');
       });
 
@@ -27,9 +28,7 @@ const Form = () => {
       <input type="text" name="name" placeholder="Your Name" required />
       <input type="email" name="email" placeholder="Your Email" required />
       <textarea name="message" placeholder="Your Message" required></textarea>
-      <button type="submit" onSubmit={handleSubmit}>
-        Submit
-      </button>
+      <button type="submit">Submit</button>
       <span id="msg"> {message} </span>
     </FormDiv>
   );
